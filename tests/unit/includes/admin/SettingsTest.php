51f696644d974ace803b184dbbb1ef4420a0316d
Adam Richards <surrealwebs@gmail.com>
2019-07-11 01:46:30 -0500

    added comments to test class

--- tests/unit/includes/admin/SettingsTest.php
+++ tests/unit/includes/admin/SettingsTest.php
@@ -1,23 +1,44 @@
 <?php
+/**
+ * Test the Admin Settings object.
+ *
+ * @package SurrealwebsPrimaryTaxonomy
+ */
 
 namespace SurrealwebsPrimaryTaxonomyTests;
 
 use Surrealwebs\PrimaryTaxonomy\Admin\Settings;
 use WP_Mock\Tools\TestCase;
 
+/**
+ * Class SettingsTest
+ *
+ * @package SurrealwebsPrimaryTaxonomy
+ */
 class SettingsTest extends TestCase {
-
+	/** @var array $default_settings Default settings for the SUT instance. */
 	protected $default_settings = [];
+
+	/** @var string $test_option_name Fake option name for use with SUT. */
 	protected $test_option_name = 'test_settings_options';
 
+	/**
+	 * Setup method.
+	 */
 	public function setUp(): void {
 		\WP_Mock::setUp();
 	}
 
+	/**
+	 * Teardown method.
+	 */
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
 	}
 
+	/**
+	 * Test the SUT's contruction, make sure data is put where it belongs.
+	 */
 	public function test_construction() {
 		\WP_Mock::userFunction(
 			'get_option' ,
@@ -36,6 +57,9 @@ class SettingsTest extends TestCase {
 		$this->assertEquals( $this->default_settings, $sut->get_default_settings() );
 	}
 
+	/**
+	 * Test that data is loaded correctly.
+	 */
 	public function test_load() {
 		$expected = 'test this is set';
 
